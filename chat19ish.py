62a9436f67db5ddcf7da4cac6bfdfbdc0941a5ca
Bruce Jensen <flaco.jensen@gmail.com>
2019-06-21 12:41:43 -0400

    Initial file setup
    Starting this as a Git project on Folio box

---
+++ chat19ish.py
@@ -0,0 +1,110 @@
+import pyperclip, re, string, time, pyautogui, webbrowser, titlecaseMod
+#TODO: Make a module that massages the library names, keeping that out of this script
+text = pyperclip.paste()
+pyautogui.FAILSAFE = True
+from titlecaseMod import titlecase
+
+patname = 'friend'
+patmail = 'Zero'
+patlibrary = 'Stax'
+ipaddress = 'No IP shown'
+gatewayURL = 'placehold'
+quest = 0
+prefgreeting = ''
+time = ' this evening'
+offer = 'I\'ll try to help you with that!'
+        
+def pickup() :
+    pyautogui.moveTo(32, 165)    #for 75% on Folio
+    pyautogui.click()    #hits the 'New' tab
+    pyautogui.PAUSE = .8   #how long does it take to load/switch tabs?
+    pyautogui.moveTo(32, 225)   #for 75%  :USE pyautogui.position()
+    pyautogui.click()   #picks up caller
+
+def grab_deets() :
+    pyautogui.moveTo(32, 600)
+  #  pyautogui.click()
+    pyautogui.PAUSE = 2.7     #how long does patron take to load?
+    pyautogui.click()
+    pyautogui.PAUSE = .1     #need to be on the new patron, and Info tab
+    pyautogui.hotkey('ctrl', 'a')    #(possibly not automatable...)
+    pyautogui.hotkey('ctrl', 'c')
+    s = pyperclip.paste()
+    return s
+
+def log_stuff(s):
+    f = open("openings.txt", "a+")
+    s = pyperclip.paste() 
+    f.write("\r\n" + s + "\r\n" + "-------------------------------------------------------")
+    f.close()
+
+# def greeting() :
+
+def split2list(x):
+    alist = x.split('\n')
+    del alist[0:10] 
+    return alist
+
+def iznta_question() :
+    if 'Qwidget:' in words :
+        qwposi = words.index('Qwidget:')
+        print (qwposi)
+        print(len(words) - qwposi - 1)
+        if (len(words) - qwposi) <= 4 :
+            return True
+        else :
+            return False
+
+def emailask():
+    if patmail != 'They gave us no address' :
+        return ''
+    else:
+        return " And though you're not obliged to give an email address, which we certainly <b><i>never</i></b> use to track or to spam anyone, it does help us serve you better. Is there an address where we can reach you in case we're disconnected and another librarian needs to follow up?"
+
+def pastegreeting() :
+    pyautogui.moveTo(1000, 450)
+    pyautogui.click()
+    pyautogui.hotkey('ctrl', 'v')
+
+pickup()
+text = grab_deets()
+log_stuff(text)
+mylist = split2list(text)
+for lines in reversed(mylist):
+    words = lines.split()
+    if len(words) == 0 : continue
+    if words[0] == 'Name:' :
+        patname = words[1]  #KICK IN TO A DIFFT FUNCTION!
+    if words[0] == 'Queue:' :
+        if 'UK' in words :
+            time = '' 
+    if words[0] == 'Patron:' :
+        if patname == 'friend':
+            patname = words[1]
+            patname = patname.capitalize()
+        patmail = words[-1]
+        if patmail == 'provided' :
+            patmail = 'They gave us no address'
+        patmail = patmail[1:-1]   #TRIMS PARENTHESES OFF BOTH ENDS, or does it????
+    if words[0] == "Patron\'s" :
+        patlibrary = (words[2: ])   #IF THIS CONTAINS 'ASHFORD' LOOK FOR NAME & EMAIL BELOW
+        if 'Ashford' in patlibrary:
+            prefgreeting = "Hi, welcome to Ashford University's 24/7 Library Chat service! " 
+    if iznta_question() :
+        offer = 'What can I help you with?'
+        continue
+if patname == 'Library' : patname = 'friend'
+if patmail == 'rovided' : patmail = 'They gave us no address'
+patlibraryX = '' 
+for kotoba in patlibrary:
+    brack = ['(',')']
+    if brack[0] in kotoba or brack[1] in kotoba  :
+        kotoba = ''
+    patlibraryX = patlibraryX + ' ' + kotoba
+    patlibraryX = titlecase(patlibraryX)
+addAsk = emailask()
+greeting = (prefgreeting + "Hello, " + patname + ", and welcome! It's great to be able to serve you" + time + ". I'm Bruce, part of a network of librarians assisting our" + patlibraryX + " colleagues while they're busy with other things. " + offer + addAsk)
+pyperclip.copy(greeting)
+log_stuff(greeting)
+pastegreeting()
+print(greeting)
