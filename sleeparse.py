e15c1ede6da9f26bc5b6c9b4e6fb5e156edea2e0
Ryujin <flaco.jensen@gmail.com>
2019-07-12 16:22:37 -0400

    Writes & reads from config.ini
    But does not yet present configuration as an option. Also, still writes only mouse click coordinates at this point.

--- sleeparse.py
+++ sleeparse.py
@@ -1,19 +1,17 @@
 import pyperclip, re, string, keyboard, time, pyautogui, webbrowser, titlecaseMod, pynput, threading, tkinter, configparser
-#CAN main() BE KICKED OFF BY A KEYBOARD COMMAND, PROGRAM 'SLEEPS' B/N INVOCATIONS, VARS DEFINED IN HEAD SECTION?
-#ABOVE, POSSIBLY THROUGH pyhook ?
 #NOW THAT WE CAN CAPTURE MOUSE CLICKS & WRITE THEM TO pyautogui/pyperclip FUNCTIONS, HOW TO STORE THOSE THROUGH
 #MULTIPLE RUNS OF THE PROGRAM?
 #MOUSE MAPPING AS AN *OPTIONAL* STEP UPON LAUNCH--NEED TO
 #SAVE 'CONFIG' FILE/FUNCTION' TO DISK
 #ANOTHER THING: SHOULD BE SIMPLE TO SCRAPE LOCATION FROM, E.G. https://www.ip2location.com/demo/172.91.22.94
-import chatglobs # THIS TO BE REPLACED BY config.ini
+config = configparser.ConfigParser()
 text = pyperclip.paste()
 pyautogui.FAILSAFE = True
 from titlecaseMod import titlecase
 from pynput.mouse import Listener
 from tkinter import *
 count = 0 
-config = configparser.ConfigParser()
+cfg = configparser.ConfigParser()
 xari = 206 ; yari = 207  
 #Then these get set & saved as 'constants', but it's only happening below, not above!
 def on_click(x, y, button, pressed) :
@@ -25,23 +23,8 @@ def on_click(x, y, button, pressed) :
         print(count)
         break
     xari2 = x ; yari2 = y
-    g = open("/usr/local/lib/python3.6/dist-packages/chatglobs/__init__.py", "a+", encoding="utf-8")
-    g.write("\r\n" + "x = " + str(x) +  ", y = " + str(y) + "\r\n")
-    g.close()
     window.after(2000, window.destroy)    
     listener.stop()
-'''
-TODO: TEST THESE COORDS, LOOK AT USE OF SCREEN SIZE
----Goes into pickup()---
-with Listener(on_click=on_click) as listener:
-    listener.join()
-pyautogui.moveTo(xari, yari)
-pyautogui.moveTo(xari, yari*1.364)
----In grab_deets---
-pyautogui.moveTo(xari, yari*3.6)
----In paste_greeting---
-pyautogui.moveTo(yari*6.061, yari*2.73)
-'''
 
 def main():
     global xari
@@ -58,15 +41,23 @@ def main():
             
     def pickup() :
         global xari, yari, xari2, yari2, xari0, yari0
-        pyautogui.moveTo(x0, y0)    #for Practice gui: 23, 194
+        cfg.read('config.ini')
+        xtab = cfg['tab']['x']
+        ytab = cfg['tab']['y']
+        print('xtab is ' + str(xtab))
+        pyautogui.moveTo(int(xtab), int(ytab))    
         pyautogui.click()    #hits the 'New' tab
         pyautogui.PAUSE = .8   #how long does it take to load/switch tabs?
-        pyautogui.moveTo(x1, y1)   #for 75%  :USE pyautogui.position()  # 23, 241
+        global xpu, ypu
+        xpu = cfg['pickup']['x']
+        ypu = cfg['pickup']['y']
+        print('ypu is ' + str(ypu))
+        pyautogui.moveTo(int(xpu), int(ypu))   #for 75%  :USE pyautogui.position()  # 23, 241
         pyautogui.click()   #picks up caller
 
     def grab_deets() :
-        global x1, y1
-        pyautogui.moveTo(x1, y1*1.7)       # 23, 421 yari2*2.17   #LOOK CAREFULLY AT THIS; CUT
+        xpu, ypu
+        pyautogui.moveTo(int(xpu), int(ypu)*1.7)       # 23, 421 yari2*2.17   #LOOK CAREFULLY AT THIS; CUT
         pyautogui.PAUSE = 2.7     #how long does patron take to load?      
         pyautogui.click()
         pyautogui.PAUSE = .1     #need to be on the new patron, and Info tab
@@ -103,7 +94,10 @@ def main():
             return " And though you're not obliged to give an email address, which we certainly <b><i>never</i></b> use to track or to spam anyone, it does help us serve you better. Is there an address where we can reach you in case we're disconnected and another librarian needs to follow up?"
 
     def pastegreeting() :
-        pyautogui.moveTo(x2, y2)     # (xari*6.061, yari*2.73)       # 700, 360
+        cfg.read('config.ini')
+        xgreet = cfg['greet']['x'] ; ygreet = cfg['greet']['y']
+        print('ygreet is ' + str(ygreet))
+        pyautogui.moveTo(int(xgreet), int(ygreet))      
         pyautogui.click()
         pyautogui.hotkey('ctrl', 'v')
 
@@ -129,7 +123,7 @@ def main():
             patmail = words[-1]
             if patmail == 'provided' :
                 patmail = 'They gave us no address'
-            patmail = patmail[1:-1]   #TRIMS PARENTHESES OFF BOTH ENDS, or does it????
+            patmail = patmail[1:-1]   #TRIMS PARENTHESES OFF BOTH ENDS 
         if words[0] == "Patron\'s" :
             patlibrary = (words[2: ])   #IF THIS CONTAINS 'ASHFORD' LOOK FOR NAME & EMAIL BELOW
             if 'Ashford' in patlibrary:
@@ -160,7 +154,6 @@ def main():
     log_stuff(greeting)
     pastegreeting()
     print(greeting)
-    print('AddAsk says ' + addAsk)
     if addAsk != '' :
         pyperclip.copy(addAsk)
         mailAlert = Tk()
@@ -173,6 +166,7 @@ def main():
         mailAlert.after(8500, mailAlert.destroy)
         mailAlert.mainloop()
 
+#cfg = configparser.ConfigParser()
 window = Tk()
 window.title("Step one!")
 window.geometry('350x80+300+225')
@@ -183,8 +177,11 @@ with Listener(on_click=on_click) as listener:
     window.mainloop()
     print('1rd: ' + str(xari2) + ' ' + str(yari2))
     x0 = xari2  ;   y0 = yari2
+    cfg['tab'] = {'x': x0, 'y': y0}
+#    with open('config.ini', 'w') as configfile:
+ #       cfg.write(configfile)
     listener.join()
-    print(str(xari2) + ', ' + str(yari2) + ' the firsd')
+   
 #    listener.stop()
         
 window = Tk()
@@ -197,6 +194,9 @@ with Listener(on_click=on_click) as listener:
     window.mainloop()
     print('2nd: ' + str(xari2) + ' ' + str(yari2))
     x1 = xari2  ;   y1 = yari2
+    cfg['pickup'] = {'x': x1, 'y': y1}
+  #  with open('config.ini', 'a') as configfile:
+   #     cfg.write(configfile)
     listener.join()
     print(str(xari2) + ', ' + str(yari2) + ' the second')
 
@@ -210,21 +210,13 @@ with Listener(on_click=on_click) as listener:
     window.mainloop()
     print('3rd: ' + str(xari2) + ' ' + str(yari2))
     x2 = xari2  ;   y2 = yari2
+    cfg['greet'] = {'x': x2, 'y': y2}
+    with open('config.ini', 'w') as configfile:
+        cfg.write(configfile)
     listener.join()
     print(str(xari2) + ', ' + str(yari2) + ' the thirnd')
-
-window = Tk()
-window.title("Step fouree!")
-window.geometry('350x80+300+225')
-window.lift()
-lbl = Label(window, text="There is no step fourx")
-lbl.grid(column=0, row=0)
-with Listener(on_click=on_click) as listener:
-    window.mainloop()
-    print('4rd: ' + str(xari2) + ' ' + str(yari2))
-    listener.join()
-    print(str(xari2) + ', ' + str(yari2) + ' the fourth')
-
+    listener.stop()
+#print (str(x0) + ', ' str(y0) + ' and ' str(x1) + ', ' str(y1) + ' and ' str(x2) + ', ' str(y2)) 
 print('Finish line!')
 if __name__ == "__main__":
     main()
