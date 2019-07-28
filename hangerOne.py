1a91bdfb52b850e8670f8fe1a7cf987c64c383a0
Ryujin <flaco.jensen@gmail.com>
2019-07-28 15:16:36 -0400

    Button choice; proper prompts & config.ini writing
    Went back to first principles, trying to understand how pynput handles Listeners and threads. Not convinced that I do. Nonetheless, this works.

--- hangerOne.py
+++ hangerOne.py
@@ -11,77 +11,104 @@ cfg = configparser.ConfigParser()
 count = 3
 config_ran = False
 lbl = Label(master, text="None")
-# from https://stackoverflow.com/questions/4103773/efficient-way-of-having-a-function-only-execute-once-in-a-loop
-# maybe learn about decorators
+v = StringVar()
+Label(master, textvariable=v).pack()
 
 def on_click3(x, y, button, pressed) :
     global xari2, yari2, xari0, yari0, lbl
-    count = 3 
+    count = 2 
     while count > 0:
         if count == 2:
             xari2 = x
             yari2 = y
             write_config3(xari2, yari2)
         count -= 1
+    listener3.stop()
     print("Third Coordinates are " + str(x) + " and " + str(y))
     print("long, winding road")
+    master.destroy()
+    ###This is the place for an acknowledgment message, further instructions, option to redo configuration
 
+def write_config3(x_coord, y_coord):
+    global configfile_name, config_ran
+    if config_ran:
+        return
+    c = open(configfile_name, "a", encoding="utf-8")
+    c.write('x=' + str(x_coord) + '\r')
+    c.write('y=' + str(y_coord))
+    c.write('\r\n')
+    print(str(c))
+    c.close()
+    config_ran = True
+    
 def write_section3(section_name):
     global configfile_name
     c = open(configfile_name, "a", encoding="utf-8")
     c.write('[' + section_name + '] \r')
     c.close()
-    print("Okay, that's it!")
-
-def crush3():
-    master.title("Third Step 3 3 3 3 3!")
-    listener3 = Listener(on_click3=on_click3)
+    
+def write_section3(section_name):
+    global configfile_name, config_ran
+    listener2.stop()
+    config_ran=False
     listener3.start()
-    write_section3("pickup")
-    listener3.stop() 
-
+    c = open(configfile_name, "a", encoding="utf-8")
+    c.write('[' + section_name + '] \r')
+    c.close()
+        
+def crush3():
+    lbl.destroy()
+    v.set("A message for everyone.")
+    master.title("Third Step 3 3 3 3 3!")
+    lbl3 = Label(master, text="Now, click in the message box...", font=("Helvetica", 14))
+    lbl3.pack()
+    write_section3("greet")
+    
 def on_click2(x, y, button, pressed) :
     global xari2, yari2, xari0, yari0, lbl
-    count = 3 
+    count = 2 
     while count > 0:
         if count == 2:
             xari2 = x
             yari2 = y
             write_config2(xari2, yari2)
         count -= 1
-    print("2nd Coordinates are " + str(x) + " and " + str(y))
-    crush3() #sort of wo
+        print("2nd Coordinates are " + str(x) + " and " + str(y))
+        listener2.stop()
 
 def write_config2(x_coord, y_coord):
-    global configfile_name
-    c = open(configfile_name, "a", encoding="utf-8")
-    c.write('x=' + str(x_coord) + '\r')
-    c.write('y=' + str(y_coord))
-    c.write('This was config2!')
-    c.write('\r\n')
-    print(str(c))
-    c.close()
+    global configfile_name, config_ran
+    if config_ran :
+        return
+    else:
+        c = open(configfile_name, "a", encoding="utf-8")
+        c.write('x=' + str(x_coord) + '\r')
+        c.write('y=' + str(y_coord))
+        c.write('\r\n')
+        print(str(c))
+        c.close()
+        config_ran = True
+        crush3()
 
 def write_section2(section_name):
-    global configfile_name
+    global configfile_name, config_ran
+    listener.stop()
+    config_ran=False
+    listener2.start()
     c = open(configfile_name, "a", encoding="utf-8")
     c.write('[' + section_name + '] \r')
     c.close()
-    write_config(x,y)
-    # Must jump into write_config2 from here
-    #crush3() doesn't quite work 
-
+    
 def crush2():
     global lbl   ###no no no no
-    #  listener.stop()
-    lbl.destroy()
+    global config_ran
+    config_ran = False
+    listener.stop()
     master.title("Step two!")
-    lbl = Label(master, text="Now to click below the linke")
-    lbl.pack()
-    listener2 = Listener(on_click2=on_click2)
-    listener2.start()
+    v.set("Does it look bad, Padre?")
+    lbl2 = Label(master, text="Now to click below the linke", font=("Helvetica", 14))
+    lbl2.pack()
     write_section2("pickup")
-    #listener2.stop() 
 
 def write_config(x_coord, y_coord):
     global configfile_name, config_ran
@@ -94,13 +121,14 @@ def write_config(x_coord, y_coord):
     print(str(c))
     c.close()
     config_ran = True
+    listener.stop()
     crush2()    # NO EFFECT
 
 def matin():
     print("Space Force!")
 
 def on_click(x, y, button, pressed) :
-    global xari2, yari2, xari0, yari0, lbl, listener
+    global xari2, yari2, xari0, yari0, lbl
     count = 3 
     while count > 0:
         if count == 2:
@@ -108,26 +136,20 @@ def on_click(x, y, button, pressed) :
             yari2 = y
             write_config(xari2, yari2)
         count -= 1
-  #  listener.stop()   # doesn't work here
- #   crush2() #sort of works almost
-    # doesn't quite work, either crush3()
-#here it needs to write the coordinates ONCE and then move along to next section!   
+
+listener = Listener(on_click=on_click)
+listener2 = Listener(on_click=on_click2)
+listener3 = Listener(on_click=on_click3)
 
 def write_section(section_name):
     global configfile_name
     c = open(configfile_name, "w", encoding="utf-8")
     c.write('[' + section_name + '] \r')
     c.close()
-    #crush2()   DOES NOT WORK!
-
-def write_prelude(v):   
-    #with Listener(on_click=on_click) as listener:
-    listener = Listener(on_click=on_click)
     listener.start()
-        #listener.join()
-    write_section(v) 
-   # listener.stop()   ### CANNOT WORK HERE
 
+def write_prelude(v):
+    write_section(v) 
     
 def crush():
     frame.destroy()
@@ -137,16 +159,8 @@ def crush():
     master.title("Map your screen")
     lbl = Label(master, text="With the QP chat interface screen up,\nclick the 'New' tab at upper left above the blue bar", font=("Helvetica", 14))
     lbl.pack()
-    print("label is a " + str(type(lbl)))
     write_prelude('tab')
-    #listener.stop()  ## now, it doesn't :-(   ### sort of works here...
-
-    '''
-    master.title("Step two")
-    lbl2 = Label(master, text="Now, click just below the blue bar", font=("Helvetica", 14))
-    lbl2.pack()
-    write_prelude('pickup')
-'''
+    
 def popupmsg(msg):
     global master
     master.destroy
@@ -158,7 +172,7 @@ def popupmsg(msg):
     B1.pack()
     popup.mainloop()
 
-if not os.path.isfile('config1.ini'):
+if not os.path.isfile('config.ini'):
     print("In this case, we must run configuration")   
 else:
     frame = Frame(master, borderwidth="7", relief="sunken")
